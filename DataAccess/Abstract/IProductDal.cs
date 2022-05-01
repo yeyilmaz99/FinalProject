34e123a038be4f64faa50d14390d2308e8e926b5
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-01 14:50:51 +0300

    8. gün Kamp

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -9,10 +9,6 @@ namespace DataAccess.Abstract
 {
     public interface IProductDal
     {
-        List<Product> GetAll();
-        void Add(Product product);
-        void Update(Product product);
-        void Delete(Product product);
-        List<Product> GetAllByCategory(int categoryId);
+
     }
 }
