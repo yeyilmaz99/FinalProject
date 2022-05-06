e95b28cb47472e820add52bd04c12a02e2f6f888
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-06 18:00:47 +0300

    9.gün kamp

--- DataAccess/Abstract/IOrderDal.cs
+++ DataAccess/Abstract/IOrderDal.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,8 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    internal interface IOrderDal
+    public interface IOrderDal: IEntityRepository<Order>
     {
+
     }
 }
--- Entities/Concrete/Order.cs
+++ Entities/Concrete/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace Entities.Concrete
 {
-    public class Order
+    public class Order:IEntity
     {
+        public int OrderId { get; set; }
+        public string CustomerId { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string ShipCity { get; set; }
     }
 }
