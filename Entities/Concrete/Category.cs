f79fe16670ce8980ef1e1eb023b51591aff43203
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-06 18:21:58 +0300

    9.gün kamp

--- Entities/Concrete/Category.cs
+++ Entities/Concrete/Category.cs
@@ -9,7 +9,7 @@ namespace Entities.Concrete
 {
     public class Category: IEntity
     {
-        public string CategoryId { get; set; }
+        public int CategoryId { get; set; }
         public string CategoryName { get; set; }
     }
 }
