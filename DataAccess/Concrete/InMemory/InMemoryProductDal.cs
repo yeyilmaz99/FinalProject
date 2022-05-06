d5f6c28a7578b573010929d6f0a93e3abd1ae110
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-06 17:51:04 +0300

    9.gün kamp

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -1,85 +1,85 @@
-﻿using DataAccess.Abstract;
-using Entities.Concrete;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using DataAccess.Abstract;
+//using Entities.Concrete;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Linq.Expressions;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace DataAccess.Concrete.InMemory
-{
-    public class InMemoryProductDal : IProductDal
-    {
-        List<Product> _products;
-        public InMemoryProductDal()
-        {
-            //Oracle, Sql Server, Postgres, MongoDb
-            _products = new List<Product>() {
-                new Product(){ProductId =1, CategoryId=1, ProductName= "Bardak",UnitPrice=15,UnitInStock=15},
-                new Product(){ProductId =2, CategoryId=1, ProductName= "Klavye",UnitPrice=500,UnitInStock=3},
-                new Product(){ProductId =3, CategoryId=2, ProductName= "Telefon",UnitPrice=1500,UnitInStock=2},
-                new Product(){ProductId =4, CategoryId=2, ProductName= "Monitör",UnitPrice=150,UnitInStock=65},
-                new Product(){ProductId =5, CategoryId=2, ProductName= "Masa",UnitPrice=85,UnitInStock=1},
+//namespace DataAccess.Concrete.InMemory
+//{
+//    public class InMemoryProductDal : IProductDal
+//    {
+//        List<Product> _products;
+//        public InMemoryProductDal()
+//        {
+//            //Oracle, Sql Server, Postgres, MongoDb
+//            _products = new List<Product>() {
+//                new Product(){ProductId =1, CategoryId=1, ProductName= "Bardak",UnitPrice=15,UnitInStock=15},
+//                new Product(){ProductId =2, CategoryId=1, ProductName= "Klavye",UnitPrice=500,UnitInStock=3},
+//                new Product(){ProductId =3, CategoryId=2, ProductName= "Telefon",UnitPrice=1500,UnitInStock=2},
+//                new Product(){ProductId =4, CategoryId=2, ProductName= "Monitör",UnitPrice=150,UnitInStock=65},
+//                new Product(){ProductId =5, CategoryId=2, ProductName= "Masa",UnitPrice=85,UnitInStock=1},
 
-            };
-        }
-        public void Add(Product product)
-        {
-            _products.Add(product);
-        }
+//            };
+//        }
+//        public void Add(Product product)
+//        {
+//            _products.Add(product);
+//        }
 
-        public void Delete(Product product)
-        {
-            //LINQ - Language Integrated Query  (dile gömülü sorgulama)
+//        public void Delete(Product product)
+//        {
+//            //LINQ - Language Integrated Query  (dile gömülü sorgulama)
 
-            //Gönderdiğim ürün id'sine sahip olan listedeki ürünü bul
-            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+//            //Gönderdiğim ürün id'sine sahip olan listedeki ürünü bul
+//            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
 
-            _products.Remove(productToDelete);
+//            _products.Remove(productToDelete);
 
 
-            //Typescriptte product listemden bu şekilde siliyordum :)
-            //foreach (var p in _products)
-            //{
-            //    if (p.ProductId == product.ProductId)
-            //    {
-            //        productToDelete = p;
-            //    }
+//            //Typescriptte product listemden bu şekilde siliyordum :)
+//            //foreach (var p in _products)
+//            //{
+//            //    if (p.ProductId == product.ProductId)
+//            //    {
+//            //        productToDelete = p;
+//            //    }
 
-            //}
-        }
+//            //}
+//        }
 
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            throw new NotImplementedException();
-        }
+//        public Product Get(Expression<Func<Product, bool>> filter)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public List<Product> GetAll()
-        {
-            return _products;
-        }
+//        public List<Product> GetAll()
+//        {
+//            return _products;
+//        }
 
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            throw new NotImplementedException();
-        }
+//        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public List<Product> GetAllByCategory(int categoryId)
-        {
-            return _products.Where(p => p.CategoryId == categoryId).ToList();
+//        public List<Product> GetAllByCategory(int categoryId)
+//        {
+//            return _products.Where(p => p.CategoryId == categoryId).ToList();
 
-        }
+//        }
 
-        public void Update(Product product)
-        {
-            //Gönderdiğim ürün id'sine sahip olan listedeki ürünü bul
-            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
-            productToUpdate.ProductName = product.ProductName;
-            productToUpdate.CategoryId = product.CategoryId;
-            productToUpdate.UnitPrice = product.UnitPrice; 
-            productToUpdate.UnitInStock = product.UnitInStock;
+//        public void Update(Product product)
+//        {
+//            //Gönderdiğim ürün id'sine sahip olan listedeki ürünü bul
+//            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+//            productToUpdate.ProductName = product.ProductName;
+//            productToUpdate.CategoryId = product.CategoryId;
+//            productToUpdate.UnitPrice = product.UnitPrice; 
+//            productToUpdate.UnitInStock = product.UnitInStock;
 
-        }
-    }
-}
+//        }
+//    }
+//}
