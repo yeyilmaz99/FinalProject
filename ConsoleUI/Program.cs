d39d97d1f81c46d3a18740ab9185d3d3432ca89c
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-13 01:38:26 +0300

    10.gün kamp

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -29,9 +29,9 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetails())
+            foreach (var product in productManager.GetProductDetails().Data)
             {
-                Console.WriteLine(product.ProductName + "/" + product.CategoryName );
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
             }
         }
     }
