0e6f374d782b28bc842adf2709d680f6acefb7c5
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-12 23:57:43 +0300

    10. gün kamp

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -15,6 +15,8 @@ namespace Business.Abstract
         List<Product> GetByUnitPrice(decimal min, decimal max);
         List<ProductDetailDto> GetProductDetails();
 
+        Product GetById(int productId);
+
         void Add(Product product);
     }
 }
