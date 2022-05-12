88bbff90c1f4d1ebcca4bf823a7cb2281ec1cdc1
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-13 01:42:37 +0300

    10.gün kamp

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -59,6 +59,10 @@ namespace Business.Concrete
 
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
+            if (DateTime.Now.Hour == 01)
+            {
+                return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
+            }
             return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails()); 
         }
     }
