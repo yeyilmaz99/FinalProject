e37170895ba3a13e33baebffc643ccc28a2b4cb7
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-13 00:05:50 +0300

    10. gün kamp

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public bool Success => throw new NotImplementedException();
+
+        public string Message => throw new NotImplementedException();
+    }
+}
