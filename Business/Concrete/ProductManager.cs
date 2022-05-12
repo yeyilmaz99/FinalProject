eb067532d269435eccbcd124c245b82a29a63155
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-12 23:59:04 +0300

    10. gün kamp

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -37,7 +37,7 @@ namespace Business.Concrete
 
         public Product GetById(int productId)
         {
-            throw new NotImplementedException();
+            _productDal.Get(p => p.ProductId == productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
