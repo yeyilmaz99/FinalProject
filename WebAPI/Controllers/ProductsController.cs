207b93fbcad36c84b17c3a59bb3943d1ff229e32
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-16 20:43:15 +0300

    11.gün kamp

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        // Loosely coupled -- gevşek bağlı
+        //naming convention
+        //IoC Container-- Inversion of Control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //dependency chain
+            
+            var result = _productService.GetAll();
+
+            return result.Data;
+        }
+    }
+}
