251333449e9e2850747e7dd8d45ce3e755adfb98
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-20 03:31:15 +0300

    12.gün kamp

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -25,7 +26,7 @@ namespace Business.Concrete
 
         public IResult Add(Product product)
         {
-            //ValidationTool.Validate(new Product);
+            ValidationTool.Validate(new Product);
 
 
             _productDal.Add(product);
