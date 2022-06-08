ab7664e443d5c893eaa9adb89dbb6a6dd90e9350
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-06-08 23:41:05 +0300

    18gün kamp

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -44,7 +44,7 @@ namespace WebAPI
             //services.AddSingleton<IProductService,ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
 
-
+            services.AddCors();
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -83,6 +83,8 @@ namespace WebAPI
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
